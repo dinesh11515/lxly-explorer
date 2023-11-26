2f04112b04db47733ccf5b2ca02a083a12c13b78
Aman-Mandal <002amankumar@gmail.com>
2023-11-26 19:22:48 +0530

    Overview component added

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import Search from '@/components/search';
 import SearchBar from '@/components/UI/SearchBar';
+import Overview from '@/components/Overview/Overview';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -10,6 +11,7 @@ export default function Home() {
     <div>
       {/* <Search /> */}
       <SearchBar />
+      <Overview />
     </div>
   );
 }
