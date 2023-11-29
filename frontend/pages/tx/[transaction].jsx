dcb502f273d53b810523cea92d5bce1efc69cecb
Aman-Mandal <002amankumar@gmail.com>
2023-11-30 01:55:57 +0530

    Transaction page

--- frontend/pages/tx/[transaction].jsx
+++ frontend/pages/tx/[transaction].jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import TransactionItem from '@/components/Transaction/TransactionItem';
 import { useRouter } from 'next/router';
-import { getTransactionDetails } from '@/utils/helper';
+import {
+  getBridgedTransactionDetails,
+  getClaimedTransactionDetails,
+  getTransactionDetails,
+} from '@/utils/helper';
+import TransactionComp from '@/components/Transaction/TransactionComp';
 
 const Transaction = () => {
   const router = useRouter();
   const { transaction } = router.query;
-
 
   // @vaibhav - Add dynamic data here
   const transactionDetails = getTransactionDetails(
@@ -19,6 +23,30 @@ const Transaction = () => {
     'Ethereum'
   );
 
+  const bridgedTransactionDetails = getBridgedTransactionDetails({
+    transaction,
+    blockNum: 198212,
+    timestamp: '12th Dec 2023, 13:34:36:00 GMT+5:30',
+    data: '0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000dabad81af85554e9ae636395611c58f7ec1aaec50000000000000000000000000000000000000000000000000000000000000018',
+    from: '0xfa233b067701800b9699b333c69a9a59ced28d26',
+    network: 'Ethereum',
+    amount: '8.90 ETH',
+    gasLimit: 203.93,
+    status: 'Delivered',
+  });
+
+  const claimedTransactionDetails = getClaimedTransactionDetails({
+    transaction,
+    blockNum: 198212,
+    timestamp: '12th Dec 2023, 13:34:36:00 GMT+5:30',
+    data: '0x00000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000001000000000000000000000000dabad81af85554e9ae636395611c58f7ec1aaec50000000000000000000000000000000000000000000000000000000000000018',
+    from: '0xfa233b067701800b9699b333c69a9a59ced28d26',
+    network: 'Polygon',
+    amount: '82.90 MATIC',
+    gasLimit: 203.93,
+    status: 'Delivered',
+  });
+
   const renderTransactionItem = (icon, label, value, isTransaction = false) => (
     <TransactionItem
       icon={icon}
@@ -28,32 +56,27 @@ const Transaction = () => {
     />
   );
 
-
   return (
     <div className='w-[90%] mx-auto font-Roboto text-white mt-8'>
-      <h1 className='text-xl'>Transaction Details</h1>
+      <h1 className='text-xl mb-8'>Transaction Details</h1>
 
-      <div className='mt-8'>
-        <p className='text-[#737373] font-light mb-1'>Basic Info</p>
+      <TransactionComp
+        title={'Basic Info'}
+        transactionDetails={transactionDetails}
+        renderTransactionItem={renderTransactionItem}
+      />
 
-        <div>
-          {transactionDetails.map((group, index) => (
-            <div
-              key={index}
-              className={`${
-                group.length > 1 ? 'flex items-center gap-1 w-full' : ''
-              }`}>
-              {group.map((item, subIndex) =>
-                renderTransactionItem(
-                  item.icon,
-                  item.label,
-                  item.value,
-                  item.isTransaction
-                )
-              )}
-            </div>
-          ))}
-        </div>
+      <div className='pl-6 border-l-2 border-[#161616] flex flex-col gap-8 mt-8 pb-8'>
+        <TransactionComp
+          title={'Bridged Transaction Info'}
+          transactionDetails={bridgedTransactionDetails}
+          renderTransactionItem={renderTransactionItem}
+        />
+        <TransactionComp
+          title={'Claimed Transaction Info'}
+          transactionDetails={claimedTransactionDetails}
+          renderTransactionItem={renderTransactionItem}
+        />
       </div>
     </div>
   );
--- frontend/utils/helper.js
+++ frontend/utils/helper.js
@@ -87,3 +87,245 @@ export const getTransactionDetails = (
     ],
   ];
 };
+
+export const getBridgedTransactionDetails = ({
+  transaction,
+  blockNum,
+  timestamp,
+  data,
+  from,
+  network,
+  amount,
+  gasLimit,
+  status,
+}) => {
+  return [
+    [
+      {
+        icon: (
+          <PiArrowsLeftRightFill
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Source transaction hash',
+        value: transaction,
+        isTransaction: true,
+      },
+    ],
+    [
+      {
+        icon: (
+          <IoCube
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Block number',
+        value: blockNum,
+      },
+      {
+        icon: (
+          <AiFillClockCircle
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Created',
+        value: timestamp,
+      },
+    ],
+    [
+      {
+        icon: (
+          <MdOutlineStickyNote2
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Status',
+        value: status,
+      },
+    ],
+    [
+      {
+        icon: (
+          <MdOutlineStickyNote2
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Data',
+        value: data,
+      },
+    ],
+    [
+      {
+        icon: (
+          <IoPerson
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'From',
+        value: from,
+        isTransaction: true,
+      },
+    ],
+    [
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Network',
+        value: network,
+      },
+    ],
+    [
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Amount',
+        value: amount,
+      },
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Gas Limit',
+        value: gasLimit,
+      },
+    ],
+  ];
+};
+
+export const getClaimedTransactionDetails = ({
+  transaction,
+  blockNum,
+  timestamp,
+  data,
+  from,
+  network,
+  amount,
+  gasLimit,
+  status,
+}) => {
+  return [
+    [
+      {
+        icon: (
+          <PiArrowsLeftRightFill
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Destination transaction hash',
+        value: transaction,
+        isTransaction: true,
+      },
+    ],
+    [
+      {
+        icon: (
+          <IoCube
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Block number',
+        value: blockNum,
+      },
+      {
+        icon: (
+          <AiFillClockCircle
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Created',
+        value: timestamp,
+      },
+    ],
+    [
+      {
+        icon: (
+          <MdOutlineStickyNote2
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Status',
+        value: status,
+      },
+    ],
+    [
+      {
+        icon: (
+          <MdOutlineStickyNote2
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Data',
+        value: data,
+      },
+    ],
+    [
+      {
+        icon: (
+          <IoPerson
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'From',
+        value: from,
+        isTransaction: true,
+      },
+    ],
+    [
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Network',
+        value: network,
+      },
+    ],
+    [
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Amount',
+        value: amount,
+      },
+      {
+        icon: (
+          <FaNetworkWired
+            className={iconStyle}
+            size={15}
+          />
+        ),
+        label: 'Gas Limit',
+        value: gasLimit,
+      },
+    ],
+  ];
+};
